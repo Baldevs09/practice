282a9702851f28a24669ba4f0a97acf54d03bdd1
Baldev singh <chaudharygunjan392@gmail.com>
2025-12-27 21:39:04 +0530

    calculator created

--- Atm.py
+++ Atm.py
@@ -1,12 +1,18 @@
-Balance=float(input(" enter the amount"))
-Atm=float(input("emter the amount"))
-
-if(Atm<=0):
-    print("invalid amount")
-elif(Atm>Balance):
-    print("amount is valid",Atm)
-elif(Balance-Atm)<1000:
-    print("amount is minimum")
+num1=int(input("enter your first number="))
+num2=int(input("enter your secound number="))
+op=input("enter your op('+','-','*','/','%')=")
+if(op=='+'):
+    print("addition",num1+num2)
+elif(op=='-'):
+    print("substraction",num1-num2)
+elif(op=='*'):
+    print("multiplication",num1*num2)
+elif(op=='/'):
+    if(num2!=0):
+        print("can not divisible")
+    else:
+        print("divisible")
+elif(op=='%'):
+    print("remainder",num1%num2)
 else:
-    print("withdrawel sucessfully")
-    print("remaing amount",Balance-Atm)
+    print("invalid operator")

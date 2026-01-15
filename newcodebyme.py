6d5402c716636cebe5628d5294b8cace4dbd95d8
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-15 20:14:11 +0530

    Updated newcodebyme.py

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,15 +1,24 @@
 #QUESTION=1
-Roll_Number=int(input("Roll Number:"))
-Mathematices=int(input("Mathematices:"))
-Science=int(input("Science:"))
-English=int(input("English"))
-Total_marks=Mathematices+Science+English
+# Roll_Number=int(input("Roll Number:"))
+# Mathematices=int(input("Mathematices:"))
+# Science=int(input("Science:"))
+# English=int(input("English"))
+# Total_marks=Mathematices+Science+English
 
 
-print("Roll Number:",Roll_Number)
-print("Mathematices:",Mathematices)
-print("Science:",Science)
-print("English:",English)
-print("Total Marks:",Total_marks)
-print("Average_marks:%.2f"%((Mathematices+Science+English)/3))
-print("Student Identifier:",Roll_Number)
+# print("Roll Number:",Roll_Number)
+# print("Mathematices:",Mathematices)
+# print("Science:",Science)
+# print("English:",English)
+# print("Total Marks:",Total_marks)
+# print("Average_marks:%.2f"%((Mathematices+Science+English)/3))
+# print("Student Identifier:",Roll_Number)
+
+Principal_Amount=int(input("PrincipalAmount:"))
+Interest_Rate=float(input("InterestRate:"))
+Time_Period=int(input("Time Period:"))
+Interest=Principal_Amount*Interest_Rate*Time_Period
+print("Prinicipal Amount:",Principal_Amount)
+print("Interest Rate:",Interest_Rate)
+print("Time period:",Time_Period)
+print("Interest:%.2f"%Interest)

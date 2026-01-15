abe1979bb2bef07a2a42d7307595ab0aa296b585
Baldev Singh <chaudharygunjan392@gmail.com>
2026-01-15 20:17:54 +0530

    Uncomment code for student marks and interest calculation

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,19 +1,17 @@
 #QUESTION=1
-# Roll_Number=int(input("Roll Number:"))
-# Mathematices=int(input("Mathematices:"))
-# Science=int(input("Science:"))
-# English=int(input("English"))
-# Total_marks=Mathematices+Science+English
-
-
-# print("Roll Number:",Roll_Number)
-# print("Mathematices:",Mathematices)
-# print("Science:",Science)
-# print("English:",English)
-# print("Total Marks:",Total_marks)
-# print("Average_marks:%.2f"%((Mathematices+Science+English)/3))
-# print("Student Identifier:",Roll_Number)
-
+Roll_Number=int(input("Roll Number:"))
+Mathematices=int(input("Mathematices:"))
+Science=int(input("Science:"))
+English=int(input("English"))
+Total_marks=Mathematices+Science+English
+print("Roll Number:",Roll_Number)
+print("Mathematices:",Mathematices)
+print("Science:",Science)
+print("English:",English)
+print("Total Marks:",Total_marks)
+print("Average_marks:%.2f"%((Mathematices+Science+English)/3))
+print("Student Identifier:",Roll_Number)
+#QUESTION=2
 Principal_Amount=int(input("PrincipalAmount:"))
 Interest_Rate=float(input("InterestRate:"))
 Time_Period=int(input("Time Period:"))

ff4392eb0ff01c7d8acb9c4a54b5c141d5013506
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-16 19:07:40 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,24 +1,9 @@
-#QUESTION=1
-# Roll_Number=int(input("Roll Number:"))
-# Mathematices=int(input("Mathematices:"))
-# Science=int(input("Science:"))
-# English=int(input("English"))
-# Total_marks=Mathematices+Science+English
-
-
-# print("Roll Number:",Roll_Number)
-# print("Mathematices:",Mathematices)
-# print("Science:",Science)
-# print("English:",English)
-# print("Total Marks:",Total_marks)
-# print("Average_marks:%.2f"%((Mathematices+Science+English)/3))
-# print("Student Identifier:",Roll_Number)
-
-Principal_Amount=int(input("PrincipalAmount:"))
-Interest_Rate=float(input("InterestRate:"))
-Time_Period=int(input("Time Period:"))
-Interest=Principal_Amount*Interest_Rate*Time_Period
-print("Prinicipal Amount:",Principal_Amount)
-print("Interest Rate:",Interest_Rate)
-print("Time period:",Time_Period)
-print("Interest:%.2f"%Interest)
+#QUESTION 3
+Customer_Id=int(input("Customer Id"))
+Unit=int(input("Unit Consumed:"))
+Rate=float(input("Rate per unit:"))
+Total_Bill=Unit*Rate
+print("Customer_Id",Customer_Id)
+print("Unit Consumed:",Unit)
+print("Rate per unit:",Rate)
+print("Total_Bill:%.2f"%Total_Bill)

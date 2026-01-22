c1337aea1509dbca865ce151ea2786a78982562c
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-22 16:48:42 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,4 +1,5 @@
-QUESTION =1
-a=int(input("weekly Money:"))
-b=int(input("weeks:"))
-print("Total saving after 5 weeks:",a*b)
+QUESTION =2
+a=int(input("Total Candies:"))
+b=int(input("Friends:"))
+print("Each friend gets ",a//b,"Candies")
+print("Left Candies:",a%b)

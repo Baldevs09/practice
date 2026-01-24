eb54da365845714da16285e9b15fde8ba1a25c74
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-24 09:59:04 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,4 +1,5 @@
-QUESTION =3
-a=int(input("Fuel Efficiency:"))
-b=int(input("Fuel Amount:"))
-print("Total Distance:",a*b,"Km")
+QUESTION =4
+a=int(input("Movie Duration:"))
+b=int(input("Total Time:"))
+print("You can watch:",b//a,"Full movies.")
+print("Time left after marathon:",b%a,"minitues.")

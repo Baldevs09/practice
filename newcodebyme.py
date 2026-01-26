8b1647487fd67a70a135dcfa18254726853a0075
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-26 10:15:23 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,6 +1,6 @@
-QUESTION =5
-a=int(input("Daliy Accommodation Cost:"))
-b=int(input("Daliy food cost:"))
-c=int(input("Daliy Entertainment Cost:"))
-d=int(input("Number of Days:"))
-print("Total trip cost is :",(a+b+c)*7)
+QUESTION =6
+
+a=int(input("local currency amount:"))
+b=float(input("Conversion rate:"))
+c=int(input("Conversion fee:"))
+print("The final amount in foreign currency is:",(a*b)-c)

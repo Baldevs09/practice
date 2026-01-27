a8e2aa5f72768dbf39eac2bfd81c2c2ee30a2e81
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-27 07:18:18 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,6 +1,14 @@
-QUESTION =6
+#QUESTION =7
+target_weight = float(input("Target Weight Loss (kg): "))
+days = int(input("Days To Achieve: "))
 
-a=int(input("local currency amount:"))
-b=float(input("Conversion rate:"))
-c=int(input("Conversion fee:"))
-print("The final amount in foreign currency is:",(a*b)-c)
+# Calories needed to burn
+total_calories = target_weight * 7700
+
+# Total distance in kilometers
+total_distance = total_calories / 60
+
+# Distance per day
+km_per_day = total_distance / days
+
+print(f"You need to walk {km_per_day:.2f} kilometers each day to reach your goal.")

f8c8fb0c83e55fa89c0311f5c3d948540f8155da
Baldev singh <chaudharygunjan392@gmail.com>
2026-01-30 17:43:01 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,7 +1,5 @@
-#Question =9
-year = int(input("Enter a year: "))
 
-if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-    print("Leap Year")
-else:
-    print("Not a Leap Year")
+
+a,b,c=map(float,input().split(" "))
+d=a+b+c
+print(d)

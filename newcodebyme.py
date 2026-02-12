efb133753476ebe8ac4640da4e94bc1694bbc70d
Baldev singh <chaudharygunjan392@gmail.com>
2026-02-12 17:42:23 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,7 +1,13 @@
-
-a,b,c,d=map(float,input().split())
-print("Rate of Gadget:",b*3)
-print("Rate of Gadget:",c*3)
-print("Rate of Gadget:",d*3)
-total=((b+c+d)*a)
-print("Total Rental Gadget:",total)
+a=float(input('Price of book 1:'))
+b=float(input('price of book 2:'))
+c=float(input('price of book 3:'))
+print('Price of book 1:%.2f'%a)
+print('Price of book 2:%.2f'%b)
+print('Price of book 3:%.2f'%c)
+t=(a+b+c)
+print('Total Amount:%.2f'%t)
+if(t>500):
+    e=(t*0.10)
+print('Discount:%.2f'%e)
+f=t-e
+print('Final Amount:%.2f'%f)

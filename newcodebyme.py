80603d7c6cabea0d8e04c5e0a54b4f6642ce2af0
Baldev singh <chaudharygunjan392@gmail.com>
2026-02-13 23:35:58 +0530

    finder who is pass or fail

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,13 +1,16 @@
-a=float(input('Price of book 1:'))
-b=float(input('price of book 2:'))
-c=float(input('price of book 3:'))
-print('Price of book 1:%.2f'%a)
-print('Price of book 2:%.2f'%b)
-print('Price of book 3:%.2f'%c)
-t=(a+b+c)
-print('Total Amount:%.2f'%t)
-if(t>500):
-    e=(t*0.10)
-print('Discount:%.2f'%e)
-f=t-e
-print('Final Amount:%.2f'%f)
+a=int(input("Mathmatices:"))
+b=int(input("Science:"))
+c=int(input("English:"))
+t=a+b+c
+print("Mathmatices:",a)
+print("Science:",b)
+print("English:",c)
+print("Total Score:",t)
+if a==40:
+    print("Result:pass")
+elif b==40:
+    print("Result:pass")
+elif c==40:
+    print("Result:pass")
+else:
+    print("Result:fail")

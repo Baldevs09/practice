105664581c5626e7428c76eb07964775b2d9a1d6
Baldev singh <chaudharygunjan392@gmail.com>
2026-02-14 12:11:43 +0530

    expression solver

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,16 +1,2 @@
-a=int(input("Mathmatices:"))
-b=int(input("Science:"))
-c=int(input("English:"))
-t=a+b+c
-print("Mathmatices:",a)
-print("Science:",b)
-print("English:",c)
-print("Total Score:",t)
-if a==40:
-    print("Result:pass")
-elif b==40:
-    print("Result:pass")
-elif c==40:
-    print("Result:pass")
-else:
-    print("Result:fail")
+a=eval(input())
+print(a)

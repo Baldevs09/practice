f21de15dc42e8815269346da3c2ae815d68bbe59
Baldev singh <chaudharygunjan392@gmail.com>
2026-03-03 15:58:24 +0530

    new code

--- newcodebyme.py
+++ newcodebyme.py
@@ -1,3 +1,9 @@
-a=int(input())
-b=int(input())
-print(a-b)
+k,a,b,c,d=map(float,input().split())
+const1=a*k
+const2=b*k
+const3=c*k
+total=const1+const2+const3+(d*k)
+print(f"cost for gadget1:{const1:.2f}")
+print(f"cost for gadget2:{const2:.2f}")
+print(f"cost for gadget3:{const3:.2f}")
+print(f"Total Rental Cost:{total:.2f}")

0642b34d16ea19a354734d4e9103c27cd1e7438d
Baldev singh <chaudharygunjan392@gmail.com>
2026-03-25 20:13:38 +0530

    print list

--- python.py
+++ python.py
@@ -1,10 +1,12 @@
-lists=[]
-while True:
-    list=input("enter your items which you want:")
-    if list=="":
-        break
-    lists.append(list)
-print("shoping list--")
-for i in lists:
-    print(i,end=",")
+l = [1, 4,9,16,25,36,49,64,81,100]
 
+i = 0
+print("[", end="")
+
+while i < len(l):
+    print(l[i], end="")
+    if i != len(l) - 1:
+        print(", ", end="")
+    i += 1
+
+print("]")

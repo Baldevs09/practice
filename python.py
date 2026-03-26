c17deec94daf45cdbc711e4fb7d8272fec64fa65
Baldev singh <chaudharygunjan392@gmail.com>
2026-03-26 19:41:07 +0530

    loop

--- python.py
+++ python.py
@@ -1,12 +1,23 @@
-l = [1, 4,9,16,25,36,49,64,81,100]
-
-i = 0
-print("[", end="")
-
-while i < len(l):
-    print(l[i], end="")
-    if i != len(l) - 1:
-        print(", ", end="")
-    i += 1
-
-print("]")
+# li=[1,4,9,16,25,36,29,64,81,100]
+# print("[",end="")
+# for i in li:
+#     print(i,end="")
+#     if i!=li[-1]:
+#         print(",",end="")
+#     i+=1
+# print("]")
+# li=[1,4,9,16,25,36,29,64,81,100]
+# i=0
+# print("[",end="")
+# while i<len(li):
+#     print(i,end="")
+#     if i!=len(li)-1:
+#         print(",",end="")
+#     i+=1
+# print("]")
+# for i in range(1,11):
+#     print(i)
+i=1
+while i <= 10:
+    print(i)
+    i+=1

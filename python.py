3ba0ad15071e2e9c56c91e2d227119d7d1639f5d
Baldev singh <chaudharygunjan392@gmail.com>
2026-03-27 08:45:32 +0530

    sum of n natural number

--- python.py
+++ python.py
@@ -1,23 +1,24 @@
-# li=[1,4,9,16,25,36,29,64,81,100]
-# print("[",end="")
-# for i in li:
-#     print(i,end="")
-#     if i!=li[-1]:
-#         print(",",end="")
+# i=1
+# while i<=10:
+#     print(i)
 #     i+=1
-# print("]")
-# li=[1,4,9,16,25,36,29,64,81,100]
-# i=0
-# print("[",end="")
-# while i<len(li):
-#     print(i,end="")
-#     if i!=len(li)-1:
-#         print(",",end="")
-#     i+=1
-# print("]")
 # for i in range(1,11):
 #     print(i)
+# i=2
+# while i<=50:
+#     print(i)
+#     i+=2
+# for i in range(2,52,2):
+#     print(i)
+# n=int(input("enter your number="))
+# i=1
+# while i<=10:
+#     print(n*i)
+#     i+=1
+n=int(input("enter your number="))
+s=0
 i=1
-while i <= 10:
-    print(i)
+while i<=n:
+    s+=i
     i+=1
+print("sum=",s)

e1b8fe562567abbd368c4d14495871c6fc5b472a
Baldev singh <chaudharygunjan392@gmail.com>
2026-03-29 20:19:26 +0530

    reverse string

--- python.py
+++ python.py
@@ -1,24 +1,7 @@
-# i=1
-# while i<=10:
-#     print(i)
-#     i+=1
-# for i in range(1,11):
-#     print(i)
-# i=2
-# while i<=50:
-#     print(i)
-#     i+=2
-# for i in range(2,52,2):
-#     print(i)
-# n=int(input("enter your number="))
-# i=1
-# while i<=10:
-#     print(n*i)
-#     i+=1
-n=int(input("enter your number="))
-s=0
-i=1
-while i<=n:
-    s+=i
-    i+=1
-print("sum=",s)
+
+s=input("enter your string:")
+rev=""
+for i in s:
+    rev=i+rev
+
+print(rev)

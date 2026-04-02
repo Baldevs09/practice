0378fd006354233478bd4974d2bb796cbdfbdde6
Baldev singh <chaudharygunjan392@gmail.com>
2026-04-02 20:32:02 +0530

    new code

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
+import random
 
-s=input("enter your string:")
-rev=""
-for i in s:
-    rev=i+rev
+quotes = [
+    "Keep going, you're doing great!",
+    "Every day is a new opportunity.",
+    "Believe in yourself.",
+    "Success is built on consistency.",
+    "Small steps lead to big results."
+]
 
-print(rev)
+print(random.choice(quotes))

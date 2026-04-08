625aed88297d9442bd286d9c8094efa94f9c90c8
Baldev singh <chaudharygunjan392@gmail.com>
2026-04-08 20:46:37 +0530

    new code

--- python.py
+++ python.py
@@ -1,11 +1,12 @@
-import random
+#6
+n=int(input())
+numbers = [10, 20, 10, 30, 10, 40, 50]
+count=0
+for i in numbers:
+    if i==n:
+        count+=1
+print(count)
 
-quotes = [
-    "Keep going, you're doing great!",
-    "Every day is a new opportunity.",
-    "Believe in yourself.",
-    "Success is built on consistency.",
-    "Small steps lead to big results."
-]
 
-print(random.choice(quotes))
+
+
